ddd6348055b44754ba9510e4ad41fe3db5317570
Bahadır Eray <57098047+Bahadireray@users.noreply.github.com>
2022-09-28 16:41:53 +0300

    code update

--- app/src/main/java/com/example/bahadir_eray_bootcampfinishproject/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/bahadir_eray_bootcampfinishproject/view/fragment/SearchFragment.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.bahadir_eray_bootcampfinishproject.adapter.TopDestinationsAdapter
+import com.example.bahadir_eray_bootcampfinishproject.data.model.countries.CountriesModel
 import com.example.bahadir_eray_bootcampfinishproject.databinding.FragmentSearchBinding
+import com.example.bahadir_eray_bootcampfinishproject.service.CountriesService
 import com.example.bahadir_eray_bootcampfinishproject.viewmodel.SearchViewModel
 
 
@@ -22,6 +20,8 @@ class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: SearchViewModel
+    private var countriesModel: ArrayList<CountriesModel>? = null
+    private val countries = CountriesService()
     private var countriesAdapter = TopDestinationsAdapter(arrayListOf())
 
 
